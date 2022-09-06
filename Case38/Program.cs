a59a7297f7486fd0ed5d4aa7a2bf65a809480664
unknown <Niromedium@gmail.com>
2022-09-06 09:33:52 +0500

    add Задача 38

---
+++ Case38/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задать массив вещественных чисел. Найти разницу между максимальным и минимальным элементами массива.
+void FillArray(double[]collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 100);
+        index ++;
+    }
+}
+void PrintArray(double[]coll)
+{
+    int count = coll.Length;
+    int pos = 0;
+    Console.Write("[ ");
+    while (pos < count)
+    {
+        Console. Write(coll[pos] + " ");
+        pos ++;
+    }
+    Console.Write(" ]");
+}
+Console.WriteLine("Введите количество элементов массива: ");
+int a = int.Parse(Console.ReadLine());
+double[]array = new double[a];
+FillArray(array);
+PrintArray(array);
+double max = array[0];
+int i = 0;
+while (i < array.Length)
+{
+    if (max < array[i])
+    {
+        max = array[i];
+    }
+    i ++;
+}
+double min = array[0];
+int ind = 0;
+while (ind < array.Length)
+{
+    if (min > array[ind])
+    {
+        min = array[ind];
+    }
+    ind ++;
+}
+double div = max - min;
+Console.Write("->" + div);
