e1baa88926b88b442d98b76e4e3d734ff965bcde
unknown <Niromedium@gmail.com>
2022-09-06 08:59:00 +0500

    add Задача 34

---
+++ Case34/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задать массив, заполненный случайными положительными трехзначными числами. Написать программу, которая покажет количество четных чисел в массиве.
+
+void FillArray(int[]collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(100, 1000);
+        index ++;
+    }
+}
+void PrintArray(int[]coll)
+{
+    int count = coll.Length;
+    int pos = 0;
+    Console.Write("[ ");
+    while (pos < count)
+    {
+        Console.Write(coll[pos] + " ");
+        pos ++;
+    }
+    Console.Write(" ]");
+}
+Console.WriteLine("Введите количество элементов массива: ");
+int a = int.Parse(Console.ReadLine());
+int[]array = new int[a];
+FillArray(array);
+PrintArray(array);
+int length = array.Length;
+int j = 0;
+int count = 0;
+while (j < length)
+{
+    if (array[j]%2 == 0)
+    {
+        count = count + 1;
+    }
+    j ++;
+}
+Console.Write("->" + count);
